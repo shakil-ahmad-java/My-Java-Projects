4fcec11d84ecb580040c79e37cd3365deb8e6cd0
Shakil Ahmad <96580916+Shakil-786-Ahmad@users.noreply.github.com>
2023-03-09 12:52:38 +0530

    Delete DemoAppFeb7 directory

--- DemoAppFeb7/src/main/java/com/bkc/config/SecurityConfigInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.bkc.config;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SecurityConfigInitializer extends AbstractSecurityWebApplicationInitializer{
-
-}
